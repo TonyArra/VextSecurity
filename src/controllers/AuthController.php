deae7ffa5b3a7cc9ac5ff7e17abab85547cd7487
Vipul <vipul.lakhtariya@dev.scrumsoft.com>
2016-08-03 22:34:23 +0530

    change password policy

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -26,6 +26,7 @@ class AuthController extends Controller
 
     public function postAuthenticate()
     {
+
         $auth_field = Config::get('auth.field', 'email');
         if (Input::has($auth_field, 'password')) {
             $field = Input::get($auth_field);
@@ -34,6 +35,7 @@ class AuthController extends Controller
 
             $user = \User::where('email', $field)
                 ->first();
+
 
             if (Auth::attempt(array($auth_field => $field, 'password' => $password))) {
                 $user_data = json_decode($user->login_history);
@@ -44,14 +46,49 @@ class AuthController extends Controller
                 else
                     $login_history = Config::get('auth.user_history');
 
+                $time_period = Config::get('auth.password_expire_period');
+                $alert_days = Config::get('auth.password_expire_alert_days');
+
+                $last_updated = $login_history['last_password_change'] == '' ? date('Y-m-d h:i:s') : $login_history['last_password_change'];
+                $current_date = date('Y-m-d h:i:s');
+
+                $val1 = new \DateTime($last_updated);
+                $val2 = new \DateTime($current_date);
+
+                $diff_days = $val2->diff($val1)->format("%a");
+                $valid_day = $time_period - $alert_days;
+                if ($diff_days > $valid_day) {
+                    $login_history['isexpiring'] = 1;
+                } else {
+                    $login_history['isexpiring'] = 0;
+                }
+
+                $valid_day = $time_period;
+                if ($diff_days > $valid_day) {
+                    $login_history['isexpired'] = 1;
+                } else {
+                    $login_history['isexpired'] = 0;
+                }
+
+                if ($login_history['isexpired'] == 1) {
+                    Auth::logout();
+                    return Redirect::to('login')
+                        ->with('error', "Your password has expired. Please contact a system administrator.");
+                }
+
                 $datetime1 = strtotime(end($login_history['failed_attempt']));
                 $datetime2 = strtotime(date('Y-m-d h:i:s'));
                 $interval = abs($datetime2 - $datetime1);
                 $minutes = round($interval / 60);
 
+                $login_history['last_login'] = date('Y-m-d h:i:s');
+
                 if ($password == 'secret') {
                     $login_history['new_user'] = true;
                 } else {
+                    if (empty($login_history['last_login'])) {
+                        $login_history['last_password_change'] = date('Y-m-d h:i:s');
+                    }
                     $login_history['new_user'] = false;
                 }
                 if ($minutes > $lock_timeout) {
@@ -62,9 +99,7 @@ class AuthController extends Controller
 
                 if ($login_history['is_locked'] == 1) {
 
-                    echo "Your account has been locked. Try after $lock_timeout minutes";
-                    exit;
-
+                    Auth::logout();
                     return Redirect::to('login')
                         ->with('error', "Your account has been locked. Try after $lock_timeout minutes.")
                         ->onlyInput($auth_field);
